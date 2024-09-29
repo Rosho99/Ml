481f715b2f4db059b3f77beeda0506ec8de05078
Rosho99 <140334405+Rosho99@users.noreply.github.com>
2024-09-29 23:53:23 +0200

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -1,1 +1,7 @@
-
+#install on the local terminal: pip3 install opencv-python
+#existing name file test
+import cv2 as cv
+#img = cv.imread("path/to/image")
+img = cv.imread("c:/Users/USER/OneDrive/Desktop/corsopython/test.jpg",0) #0 is black and white
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
